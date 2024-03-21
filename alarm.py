7cdec6937bf5d96282914fb0e62aebec192df290
Adharsh Prathap <pm.adharsh@gmail.com>
2024-03-21 00:06:34 -0700

    completed formatting

--- alarm.py
+++ alarm.py
@@ -12,37 +12,43 @@ import random
 color1 = 'black'
 color2 = 'white'
 
+def get_screen_dimensions():
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    return screen_width, screen_height
+
 window = Tk()
 window.title("")
-window.geometry('350x150')
+screen_width, screen_height = get_screen_dimensions()
+window.geometry(f'{screen_width // 2}x{screen_height // 2}')  # Set window size to half of the screen
 window.configure(bg=color2)
 
-name = Label(window, text="Alarm", height=1, font=('Ivy 18 bold'), bg=color2)
-name.place(relx=0.5, y=10, anchor='center')  # Center horizontally
+name = Label(window, text="ALARM", height=1, font=('Ivy 25 bold'), bg=color2)
+name.place(relx=0.5, y=30, anchor='center')  # Center horizontally
 
-hour_label = Label(window, text="hour", height=1, font=('Ivy 10 bold'), bg=color2, fg=color1)
-hour_label.place(x=20, y=40)
+hour_label = Label(window, text="HOUR", height=1, font=('Ivy 15 bold'), bg=color2, fg=color1)
+hour_label.place(x=20, y=150)
 
-c_hour = Combobox(window, width=2, font=('arial 15'))
+c_hour = Combobox(window, width=10, font=('arial 15 bold'))
 c_hour['values'] = ("00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
 c_hour.current(0)  # Set default value to "00"
-c_hour.place(x=55, y=40)
+c_hour.place(x=90, y=150)
 
-minute_label = Label(window, text="minute", height=1, font=('Ivy 10 bold'), bg=color2, fg=color1)
-minute_label.place(x=110, y=40)
+minute_label = Label(window, text="MINUTE", height=1, font=('Ivy 15 bold'), bg=color2, fg=color1)
+minute_label.place(x=245, y=150)
 
-c_minute = Combobox(window, width=2, font=('arial 15'))
+c_minute = Combobox(window, width=10, font=('arial 15 bold'))
 c_minute['values'] = tuple(f"{i:02d}" for i in range(61))
 c_minute.current(0)  # Set default value to "00"
-c_minute.place(x=160, y=40)
+c_minute.place(x=330, y=150)
 
-period_label = Label(window, text="period", height=1, font=('Ivy 10 bold'), bg=color2, fg=color1)
-period_label.place(x=215, y=40)
+period_label = Label(window, text="PERIOD", height=1, font=('Ivy 15 bold'), bg=color2, fg=color1)
+period_label.place(x=490, y=150)
 
-c_period = Combobox(window, width=3, font=('arial 15'))
+c_period = Combobox(window, width=10, font=('arial 15 bold'))
 c_period['values'] = ("AM", "PM")
 c_period.current(0)  # Set default value to "AM"
-c_period.place(x=265, y=40)
+c_period.place(x=575, y=150)
 
 def ask_math_problem():
         num1 = random.randint(1, 10)
@@ -76,13 +82,13 @@ def deactivate_alarm():
 
 selected = IntVar()
 
-activate = Radiobutton(window, height=1, font=('arial 10 bold'), value=1, text="Activate", bg=color2,
+activate = Radiobutton(window, height=1, font=('arial 15 bold'), value=1, text="Activate", bg=color2,
                        command=activate_alarm, variable=selected)
-activate.place(relx=0.35, y=95, anchor='center')  # Center horizontally
+activate.place(relx=0.35, y=300, anchor='center')  # Center horizontally
 
-deactivate = Radiobutton(window, height=1, font=('arial 10 bold'), value=2, text="Deactivate", bg=color2,
+deactivate = Radiobutton(window, height=1, font=('arial 15 bold'), value=2, text="Deactivate", bg=color2,
                          command=deactivate_alarm, variable=selected)
-deactivate.place(relx=0.65, y=95, anchor='center')  # Center horizontally
+deactivate.place(relx=0.65, y=300, anchor='center')  # Center horizontally
 
 
 def flash_colors(window):
